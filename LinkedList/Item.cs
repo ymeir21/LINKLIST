3045e8617a191c081dd5a07e87d946fd89c1352f
ymeir21@gmail.com <ymeir21@gmail.com>
2022-01-24 11:31:32 +0200

    First commit

---
+++ LinkedList/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Item
+    {
+        public string name;
+        public Item next = null;
+        public Item prev = null;
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Progrem
+    {
+        static void Main(string[] args)
+        {
+            CustomLinkedList list = new CustomLinkedList();
+            
+
+            const int MAX = 3;
+            for (int i = 0; i< MAX;i++)
+            {
+                var str =Console.ReadLine();
+                list.AddItam(str);
+            }
+        }
+
+       
+
+    }
+    
+
+    //        static void Main(string[] args)
+    //    {
+    //        CustomLinkedList linkedList = new CustomLinkedList();
+            
+
+    //        linkedList.Add("Item1");
+    //        {
+    //            var value = Item1;
+    //        }
+    //        linkedList.Add("Item2");
+
+    //        linkedList.Delete();
+    //        linkedList.ToString();
+    //        Console 
+    //    }
+    //}
+}
